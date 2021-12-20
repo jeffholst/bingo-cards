f079ac3f18f99480f187e3f7a0b9b69f973dfd46
Jeff <jeffholst@gmail.com>
2021-12-20 10:30:07 -0600

    chore: PWA icons test 1

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,33 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),
-    VitePWA({})],
+    VitePWA({
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],  
+      manifest: {
+        name: 'Hidden Creek Ranch Bingo',
+        short_name: 'HCR Bingo',
+        description: '',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      }
+    })],
   resolve: {
       alias: [
       {
