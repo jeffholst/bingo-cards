07fa5e7b1a37552162593cadb78e6f55ac694951
Jeff <jeffholst@gmail.com>
2021-12-19 20:29:37 -0600

    chore: added env

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 // import HelloWorld from './components/HelloWorld.vue'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import Bingo from './components/Bingo.vue'
 import Scorecard from './components/Scorecard.vue'
 import Items from './components/Items.vue'
@@ -15,11 +15,13 @@ import { getScoreAPI, addScoreAPI, syncPendingCards } from './api'
 import { Auth } from "aws-amplify";
 import { useUserStore } from './stores/user'
 
+const version = import.meta.env.VITE_APP_VERSION
 const currentPage = ref('Bingo')
 const firstName = ref('')
 const lastName = ref('')
 const nickName = ref('')
 const myUser = useUserStore()
+const initials = computed(() => `${myUser.firstName.substring(0, 1)}${myUser.lastName.substring(0, 1)}`)
 
 const myTimeout = setInterval(function() {syncPendingCards(myUser.cardItems, myUser.userName)}, 5000);
 
@@ -129,12 +131,33 @@ function userSignedIn(data) {
             <!-- Profile dropdown -->
             <Menu as="div" class="ml-3 relative">
               <div>
-                <a href="#" :class="['block px-4 py-2 text-sm text-gray-700']" @click="signOut">
-                      Sign Out
-                </a>
-                
+                <MenuButton class="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                  <span class="sr-only">Open user menu</span>
+                  <div class="w-10 h-10 rounded-full inline-flex items-center justify-center bg-white text-gray-700 font-bold">
+                    {{ initials}}
+                  </div>
+                </MenuButton>
               </div>
-             
+              <transition enter-active-class="transition ease-out duration-200" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <MenuItem>
+                    <a href="#" @click="changeNav('Profile')" class="block px-4 py-2 text-sm text-gray-700">
+                      Profile
+                    </a>
+                  </MenuItem>
+                  <MenuItem>
+                    <a href="#" @click="signOut" class="block px-4 py-2 text-sm text-gray-700">
+                      Sign Out
+                    </a>
+                  </MenuItem>
+                  <MenuItem>
+                  <a href="#" class="block px-4 py-2 text-sm text-gray-700">
+                     Version: {{ version }}
+                    </a>
+                  </MenuItem> 
+                  
+                </MenuItems>
+              </transition>
             </Menu>
           </div>
           <div class="-mr-2 flex items-center sm:hidden">
@@ -154,8 +177,9 @@ function userSignedIn(data) {
             {{ item.name }}
           </DisclosureButton>
         </div>
-        <div class="pt-4 pb-3 border-t border-gray-200">
+         <div class="pt-4 pb-3 border-t border-gray-200">
           <div class="mt-3 space-y-1">
+            Version: {{ version }}
             <DisclosureButton href="#" as="a" class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100" @click="signOut">
               Sign Out
             </DisclosureButton>
@@ -163,6 +187,7 @@ function userSignedIn(data) {
         </div>
       </DisclosurePanel>
     </Disclosure>
+
 
     <div class="py-10">
       <main>
