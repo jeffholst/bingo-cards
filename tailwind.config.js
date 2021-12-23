fc8058292e35ba06d4849519d323f82f7de17a02
Jeff <jeffholst@gmail.com>
2021-12-22 20:35:02 -0600

    chore: tailwind purge actually working

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,5 @@ module.exports = {
   plugins: [
 
   ],
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-  },
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 }
