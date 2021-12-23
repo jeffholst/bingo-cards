f7cf0137e9b8d827939f47b5ccaa3257e16ede45
Jeff <jeffholst@gmail.com>
2021-12-22 20:18:56 -0600

    chore: now purging tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,4 +12,8 @@ module.exports = {
   plugins: [
 
   ],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+  },
 }
